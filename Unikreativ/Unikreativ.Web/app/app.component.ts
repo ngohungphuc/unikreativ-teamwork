0f6fd7d08c1cbbca9af4d3462f9a645cc396a247
ngohungphuc <ngohungphuc95@gmail.com>
2017-04-27 11:28:54 +0700

    Rename tag and project

---
+++ Unikreativ/Unikreativ.Web/app/app.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core'
+
+@Component({
+    selector: 'unikreativ',
+    templateUrl: 'partial/app'
+})
+export class AppComponent {
+    welcomeMsg = 'Welcome to Angular4 & ASP.Net Core'
+}
