1675d3ad0c27101d9516ce6f0f8025c28aa5428a
ngohungphuc <ngohungphuc95@gmail.com>
2017-05-31 15:22:08 +0700

    form update client info

--- Unikreativ/Unikreativ/app/app/modules/admin/user/client-edit.component.ts
+++ Unikreativ/Unikreativ/app/app/modules/admin/user/client-edit.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 import { Component, OnInit, Input } from '@angular/core'
 import { UserService } from '../../../services/index'
 import { Observable } from 'rxjs/Observable'
@@ -12,10 +13,36 @@ export class ClientEditComponent implements OnInit {
     @Input()
     client: any[]
 
+    editClientInfoForm: FormGroup
+    companyName: FormControl
+    country: FormControl
+    address: FormControl
+    email: FormControl
+    phoneNumber: FormControl
+    website: FormControl
+
     constructor(private userService: UserService) {
     }
 
     ngOnInit() {
+        this.companyName = new FormControl('', Validators.required)
+        this.country = new FormControl('', Validators.required)
+        this.address = new FormControl('', Validators.required)
+        this.email = new FormControl('', Validators.required)
+        this.phoneNumber = new FormControl('', Validators.required)
+        this.website = new FormControl('', Validators.required)
+
+
+        this.editClientInfoForm = new FormGroup({
+            companyName: this.companyName,
+            country: this.country,
+            address: this.address,
+            email: this.email,
+            phoneNumber: this.phoneNumber,
+            website: this.website
+        })
+
+
         console.log(this.client)
     }
 
