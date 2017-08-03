93591d7d70efd407e86746733e89bb66405edfc5
ngohungphuc <ngohungphuc95@gmail.com>
2017-08-03 15:27:37 +0700

    add viewmodel

---
+++ Unikreativ/Unikreativ.Entities/ViewModel/EventViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unikreativ.Entities.Entities;
+
+namespace Unikreativ.Entities.ViewModel
+{
+    public class EventViewModel
+    {
+        public EventViewModel()
+        {
+            Project = new Project();
+            TasksRequest = new TasksRequest();
+        }
+        
+        public string TaskName { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DateAssigned { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public string AssignBy { get; set; }
+
+        public string AssignTo { get; set; }
+
+        public Project Project { get; set; }
+
+        public TasksRequest TasksRequest { get; set; }
+
+    }
+}
--- Unikreativ/Unikreativ.Entities/ViewModel/ProjectViewModel.cs
+++ Unikreativ/Unikreativ.Entities/ViewModel/ProjectViewModel.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using Unikreativ.Entities.Entities;
 namespace Unikreativ.Entities.ViewModel
 {
     public class ProjectViewModel
     {
+        public ProjectViewModel()
+        {
+            Billings = new List<Billing>();
+            Events = new List<Event>();
+            SubTasks = new List<SubTask>();
+        }
         public Guid Id { get; set; }
         public string ProjectName { get; set; }
 
@@ -14,5 +21,9 @@ namespace Unikreativ.Entities.ViewModel
         public string UserName { get; set; }
 
         public User Client { get; set; }
+
+        public IList<Billing> Billings { get; set; }
+        public IList<Event> Events { get; set; }
+        public IList<SubTask> SubTasks { get; set; }
     }
 }
