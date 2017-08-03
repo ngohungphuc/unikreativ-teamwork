dbe2a060c6cdbb36f15d38e5b63e13570131e606
ngohungphuc <ngohungphuc95@gmail.com>
2017-08-03 14:14:50 +0700

    user resolver services

---
+++ Unikreativ/Unikreativ/Helper/Auth/UserResolverService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Unikreativ.Entities.Entities;
+
+namespace Unikreativ.Helper.Auth
+{
+    public class UserResolverService
+    {
+        private readonly IHttpContextAccessor _context;
+        private readonly UserManager<User> _userManager;
+        public UserResolverService(IHttpContextAccessor context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<User> GetUser() => await _userManager.FindByNameAsync(_context.HttpContext.User?.Identity?.Name);
+
+        public string GetUserName() => _context.HttpContext.User?.Identity?.Name;
+
+        public async Task<string> GetUserId()
+        {
+            var user =  await _userManager.FindByNameAsync(_context.HttpContext.User?.Identity?.Name);
+            return user.Id;
+        }
+    }
+}
