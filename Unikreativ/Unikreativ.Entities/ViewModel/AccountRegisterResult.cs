6e55272618f08d3ca2999eb78eba13b61f5364cc
ngohungphuc <ngohungphuc95@gmail.com>
2017-09-05 17:52:28 +0700

    refactor code

---
+++ Unikreativ/Unikreativ.Entities/ViewModel/AccountRegisterResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unikreativ.Entities.ViewModel
+{
+    public enum AccountRegisterResult
+    {
+        AccountExist,
+        EmailExist,
+        Success,
+        Failed
+    }
+}
--- Unikreativ/Unikreativ/Helper/Account/AccountHelper.cs
+++ Unikreativ/Unikreativ/Helper/Account/AccountHelper.cs
@@ -10,17 +10,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Unikreativ.Entities.Entities;
 using Unikreativ.Entities.Models.AccountViewModels;
+using Unikreativ.Entities.ViewModel;
 using Unikreativ.Helper.Auth;
 using Unikreativ.Helper.Security;
 using Unikreativ.Repositories.UnitOfWork;
 namespace Unikreativ.Helper.Account
 {
-
     public interface IAccountHelper
     {
         Task UpdateUserInfoAsync(User account);
-        Task<RegisterViewModel> CreateNewAccount(dynamic accountDto);
+        Task<dynamic> CreateNewAccount(dynamic accountDto);
     }
+
     public class AccountHelper: IAccountHelper
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -48,23 +49,23 @@ namespace Unikreativ.Helper.Account
 
         public async Task UpdateUserInfoAsync(User account)
         {
-            if (account == null) throw new Exception("Account null");
+            if (account == null) throw new ArgumentNullException(nameof(account));
             await _unitOfWork.Repository<User>().UpdateAsync(account);
         }
 
-        public async Task<RegisterViewModel> CreateNewAccount(dynamic accountDto)
+        public async Task<dynamic> CreateNewAccount(dynamic accountDto)
         {
             if (accountDto == null)
                 throw new ArgumentNullException(nameof(accountDto));
 
             var account = Mapper.Map<User>(accountDto);
 
-            if (await _validateAccount.CheckAccountExist(accountDto.UserName)) throw new Exception("Account already exist");
-            if (await _validateAccount.CheckEmailExist(accountDto.Email)) throw new Exception("Email already exist");
+            if (await _validateAccount.CheckAccountExist(accountDto.UserName)) return AccountRegisterResult.AccountExist;
+            if (await _validateAccount.CheckEmailExist(accountDto.Email)) return AccountRegisterResult.EmailExist;
 
             var randomPassword = GenerateToken.RandomString();
             var result = await _userManager.CreateAsync(account, randomPassword);
-            if (!result.Succeeded) throw new Exception("Something went wrong please try again later");
+            if (!result.Succeeded) return AccountRegisterResult.Failed;
 
             var code = await _userManager.GenerateEmailConfirmationTokenAsync(account);
             var callbackUrl = _urlHelperFactory.GetUrlHelper(_actionAccessor.ActionContext)
@@ -81,4 +82,6 @@ namespace Unikreativ.Helper.Account
         }
 
     }
+
+  
 }
